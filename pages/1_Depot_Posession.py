7a021b9dcd2090d868ea54a30087881c3a64835f
Jade <jade_da@hotmail.com>
2024-05-05 12:37:08 +0800

    a

--- pages/1_Depot_Posession.py
+++ pages/1_Depot_Posession.py
@@ -84,11 +84,11 @@ for row_name, i in ProcessedDF.iterrows():
 
 #Main layout
 
-#url = 'https://www.jnnprogress.com/Site/gtest.png'
-url = 'https://img.creative.com/images/products/large/pdt_23968.png'
+url = 'https://www.jnnprogress.com/Site/gtest.png'
+#url = 'https://img.creative.com/images/products/large/pdt_23968.png'
 #url = 'https://www.google.co.th/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-#resp = requests.get(url)
-#img = BytesIO(resp.content)
+resp = requests.get(url)
+img = BytesIO(resp.content)
 #img = Image.open(img)
 #img = Image.open('images\Ann2.PNG')
 #img2 = img.copy()
