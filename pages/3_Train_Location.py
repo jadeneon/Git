c0cb1e551f6dc5e0af290f9ee4669ee053729472
jade <jade_da@hotmail.com>
2024-05-25 22:01:59 +0800

    a

--- pages/3_Train_Location.py
+++ pages/3_Train_Location.py
@@ -27,3 +27,10 @@ st.sidebar.title("Hitachi TCO &copy;")
 st.sidebar.image("./images/hitachi.png")
 
 st.markdown("# Train Location underdevelopment")
+
+
+#import cv2
+#s_img = cv2.imread("smaller_image.png")
+#l_img = cv2.imread("larger_image.jpg")
+#x_offset=y_offset=50
+#l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
