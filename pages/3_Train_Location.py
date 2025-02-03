45e3562c23fab2a0ee1c21bbff0ff7b9882784d3
jadeneon <jade_da@hotmial.com>
2025-02-03 11:12:19 +0800

    a

--- pages/3_Train_Location.py
+++ pages/3_Train_Location.py
@@ -148,7 +148,7 @@ if connFailed == False:
 
     #Image run
     Layout = cv2.imread("./images/TrainLoc.PNG")
-    MLLayout = cv2.imread("./images/mainline.PNG")
+    MLLayout = cv2.imread("./images/Mainline.PNG")
 
     #add train to gsheet and layout Depot
     for pid in Parking:
